c759fd4a2877316b06409e5a2fb2db469c93bcbf
YoungChulShin <go1323@gmail.com>
2024-10-23 19:11:21 +0900

    HelloReactor 내용 추가

--- 01.hello-reactive/src/main/java/study/spring/websocket/helloreactive/HelloReactor.java
+++ 01.hello-reactive/src/main/java/study/spring/websocket/helloreactive/HelloReactor.java
@@ -1,11 +1,20 @@
 package study.spring.websocket.helloreactive;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class HelloReactor {
 
   public static void main(String[] args) {
-    Mono.just("Hello Reactor")
-        .subscribe(System.out::println);
+    // Publisher가 데이터를 생성
+    Flux<String> sequence = Flux.just("Hello", "Reactor");
+    sequence
+        // 데이터를 가공
+        .map(data -> data.toLowerCase())
+        // 가공한 데이터를 구독
+        // data -> System.out.println(data) 이게 Subscriber
+        .subscribe(data -> System.out.println(data));
+
+    // Subscriber의 onSubscribe 호출
   }
 }
